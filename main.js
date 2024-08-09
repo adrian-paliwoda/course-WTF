b78c879c0bf20f6956a829716bca2cba7e6aa767
Adrian Paliwoda <adrian94@tutanota.com>
2024-08-09 20:30:06 +0200

    Typograficzne zmiany

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 const firstName = 'Adrian';
 const age = 30;
 
-console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 
 const header = document.querySelector('.page-header__heading--js');
 console.log(header.innerHTML);
 
-// header.innerHTML = "Hi"
+header.innerHTML = `Nazywam się <span class="special">${firstName}</span> i mam ${age} lat`;
 
 const emptyElement = document.querySelector('#empty-element');
 emptyElement.innerHTML = '<p>Pusty element</p>'
