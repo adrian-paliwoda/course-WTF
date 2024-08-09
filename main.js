2c36e7dff9a6f2606de2ffe381b15297228fe292
Adrian Paliwoda <adrian94@tutanota.com>
2024-08-09 13:03:13 +0200

    Add empty div

--- main.js
+++ main.js
@@ -3,5 +3,10 @@ const age = 30;
 
 console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 
-const header = document.querySelector('h1');
+const header = document.querySelector('.page-header__heading--js');
+console.log(header.innerHTML);
 
+header.innerHTML = "Hi"
+
+const emptyElement = document.querySelector('#empty-element');
+emptyElement.innerHTML = '<p>Pusty element</p>'
