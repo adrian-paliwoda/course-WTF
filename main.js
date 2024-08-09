3af4e1a7967ca9c997ea409b7072e73c693daaa9
Adrian Paliwoda <adrian94@tutanota.com>
2024-08-09 20:14:25 +0200

    Add new styles

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ console.log(`Nazywam się ${firstName} i mam ${age} lat`);
 const header = document.querySelector('.page-header__heading--js');
 console.log(header.innerHTML);
 
-header.innerHTML = "Hi"
+// header.innerHTML = "Hi"
 
 const emptyElement = document.querySelector('#empty-element');
 emptyElement.innerHTML = '<p>Pusty element</p>'
