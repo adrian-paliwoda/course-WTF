6e33b7e28e880cfbccaa518c30669295f0f96fd8
Adrian Paliwoda <adrian94@tutanota.com>
2024-10-20 13:15:46 +0200

    Changed styles to more tailwindcss style

--- wtfPortfolio/tailwind.config.js
+++ wtfPortfolio/tailwind.config.js
@@ -6,10 +6,20 @@ module.exports = {
       colors: {
         bg: "#020916",
         accent: "#CEFF7E",
-        description: '#9CA3AF'
+        description: '#9CA3AF',
+        lightGray: "#1F2937"
       },
       fontFamily: {
         'dmsans': ["DM Sans", 'sans-serif']
+      },
+      maxWidth: {
+        'wtf': '82rem'
+      },
+      borderRadius: {
+        'wtf': '0.625rem'
+      },
+      boxShadow: {
+        'innerlight': 'inset 0 2px 0 0 rgba(255, 255, 255, 0.15)'
       }
     },
   },
