12edd4d788eaa8a8caaa656897b03ea92f4bb307
Adrian Paliwoda <adrian94@tutanota.com>
2024-10-21 14:09:18 +0200

    Fixed couple of errors

--- wtfPortfolio/src/main.js
+++ wtfPortfolio/src/main.js
@@ -1,15 +1,84 @@
-const projectsContainer = document.getElementById('projects');
+import starSvg from "./images/star.svg";
+import githubSvg from "./images/github.svg";
+import demoSvg from "./images/view_demo.svg";
 
-const createComponent = (name, description, languages, stars, urlToRepo) => {
-    let component =  `<div><h1>${name} <span><img src"./images/star.svg"/>${stars}</span></h1><p>${description}</p><ul>`;
+const projectsContainer = document.getElementById("projects");
 
-    for(const language in languages) {
-        component += `<li>${language}</li>`
+const createComponent = (
+  name,
+  description,
+  topics,
+  stars,
+  urlToRepo,
+  homepage
+) => {
+  let topicsContent = "";
+  let demoContent = "";
+
+  if (topics.length > 0) {
+    topicsContent = '<ul class="flex gap-2 flex-wrap mb-10">';
+    for (const topic of topics) {
+      topicsContent += `<li
+          class="h-6 flex gap-0.5 items-center bg-gray-400/10 py-1 px-2 leading-none rounded text-sm font-bold"
+        >
+          ${topic}
+        </li>`;
     }
+    topicsContent += "</ul>";
+  }
 
-    component = component + `</ul><a href=""><img src"./images/view_demo.svg">View Demo</a><a href="${urlToRepo}"><img src"./images/github.svg">Source code</a></div>`;
-    return component;
-}
+  if (homepage) {
+    demoContent = ` <a
+          class="md:text-xl bg-bg text-accent border-lightGray border-2 flex gap-3 font-bold py-4 px-5 items-center rounded-wtf md:rounded-xl hover:border-accent transition-colors duration-500"
+          href="${homepage}"
+          target="_blank"
+          rel="noreferrer nofollow"
+          ><img src="${demoSvg}" class="w-6 h-6" />View
+          demo</a
+        >`;
+  }
+
+  let template = `
+    <article
+    class="bg-gradient-to-br from-white/10 to-white/5 rounded-wtf md:rounded-wtfxl shadow-innerlight ring ring-bg overflow-clip flex flex-col h-full"
+  >
+    <div
+      class="border-b border-bg h-11 p-4 bg-gradient-to-br from-white/10 to-white/5 rounded-t-wtf md:rounded-t-wtftlr shadow-innerlight flex flex-row gap-1.5"
+    >
+      <spna class="bg-bg w-3 h-3 block rounded-full opacity-50"></spna>
+      <spna class="bg-bg w-3 h-3 block rounded-full opacity-50"></spna>
+      <spna class="bg-bg w-3 h-3 block rounded-full opacity-50"></spna>
+    </div>
+    <div class="p-5 md:p-6 lg:p-10 flex flex-col justify-between grow">
+    <div>
+      <header class="flex flex-row gap-4 items-center mb-4">
+        <h3 class="text-2xl font-bold leading-none">${name}</h3>
+        <p
+          class="h-6 flex gap-0.5 items-center bg-gray-400/10 py-1 px-2 leading-none font-medium rounded"
+        >
+          <img src="${starSvg}" class="" />${stars}
+        </p>
+      </header>
+      <p class="text-gray-400 text-xl mb-4">${description}</p>
+      ${topicsContent}
+      </div>
+      <div class="flex flex-col md:flex-row gap-4 items-start mt-4 flex-wrap ">
+       ${demoContent}
+        <a
+          class="md:text-xl bg-bg text-accent border-lightGray border-2 flex gap-3 font-bold py-4 px-5 items-center rounded-wtf md:rounded-xl hover:border-accent transition-colors duration-500"
+          href="${urlToRepo}"
+          target="_blank"
+          rel="noreferrer noopener nofollow"
+          ><img src="${githubSvg}" class="w-6 h-6" />Source
+          code</a
+        >
+      </div>
+    </div>
+  </article>
+`;
+
+  return template;
+};
 
 const getProjects = async () => {
   const url = "https://api.github.com/users/adrian-paliwoda/repos";
@@ -22,26 +91,35 @@ const getProjects = async () => {
       throw new Error(message);
     }
 
-    let allComponent = '';
+    let allComponent = "";
     const result = await response.json();
-    for(const repo of result){
-        const name = repo.name;
-        const description = repo.description;
-        const languages = repo.languages;
-        const stars = repo.stargazers_count;
-        const urlToRepo = repo.clone_url;
+    for (const repo of result) {
+      const name = repo.name;
+      const description = repo.description;
+      const topics = repo.topics;
+      const stars = repo.stargazers_count;
+      const urlToRepo = repo.clone_url;
+      const homepage = repo.homepage;
 
-        const component = createComponent(name, description, languages, stars, urlToRepo);
+      if (!description) {
+        continue;
+      }
 
-        allComponent += component;
+      const component = createComponent(
+        name,
+        description,
+        topics,
+        stars,
+        urlToRepo,
+        homepage
+      );
+
+      allComponent += component;
     }
-    projectsContainer.innerHTML = allComponent;
-    
-
+    projectsContainer.insertAdjacentHTML("afterbegin", allComponent);
   } catch (error) {
     console.log("Cannot obtain projects.");
   }
 };
 
-
-
+getProjects();
--- wtfPortfolio/tailwind.config.js
+++ wtfPortfolio/tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     extend: {
       colors: {
         bg: "#020916",
-        accent: "#CEFF7E",
+        accent: {DEFAULT: "#CEFF7E", 400: '#C2FC5E'},
         description: '#9CA3AF',
         lightGray: "#1F2937"
       },
@@ -24,6 +24,9 @@ module.exports = {
       boxShadow: {
         'innerlight': 'inset 0 2px 0 0 rgba(255, 255, 255, 0.15)',
         'innerlightmini': 'inset 0 1px 0 0 rgba(255, 255, 255, 0.15)'
+      },
+      animation: {
+        slowSpin: 'spin 16s infinite linear'
       }
     },
   },
